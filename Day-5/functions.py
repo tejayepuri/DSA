c787ea8b79beda662ed54303a5212a2035259dbf
Yepuri Teja Naga Anil Kumar <126933993+tejayepuri@users.noreply.github.com>
2026-03-17 12:50:58 +0530

    Add files via upload

---
+++ Day-5/functions.py
@@ -0,0 +1,112 @@
+def calculate_area(length,breadth):
+    return length*breadth
+print(calculate_area(5,4))
+
+
+def is_prime(num):
+    if num<=1:
+        return False
+    for i in range(2,num):
+        if num%i==0:
+            return False
+    return True
+def main():
+    num=int(input("enter number:"))
+    if is_prime(num):
+        print("prime")
+    else:
+        print("not prime")
+main()
+
+
+def check_odd(num):
+    if num<=1:
+        return False
+    for i in range(2
+
+
+
+
+
+                   def is_prime(num):
+    if num<=1:
+        return False
+    for i in range(2,num):
+        if num%i==0:
+            return False
+    return True
+def main():
+    num=int(input("enter number:"))
+    if is_prime(num):
+        print("prime")
+    else:
+        print("not prime")
+main()        
+def is_prime(num):
+    if num<=1:
+        return False
+    for i in range(2,num):
+        if num%i==0:
+            return False
+    return True
+def main():
+    num=int(input("enter number:"))
+    if is_prime(num):
+        print("prime")
+    else:
+        print("not prime")
+main()        
+def is_prime(num):
+    if num<=1:
+        return False
+    for i in range(2,num):
+        if num%i==0:
+            return False
+    return True
+def main():
+    num=int(input("enter number:"))
+    if is_prime(num):
+        print("prime")
+    else:
+        print("not prime")
+main()        
+def is_prime(num):
+    if num<=1:
+        return False
+    for i in range(2,num):
+        if num%i==0:
+            return False
+    return True
+def main():
+    num=int(input("enter number:"))
+    if is_prime(num):
+        print("prime")
+    else:
+        print("not prime")
+main()        
+
+def is_prime(num):
+    if num<=1:
+        return False
+    for i in range(2,num):
+        if num%;;lo;'?,..,;./;. gg    ?i==0:
+            return False
+    return True
+def main():
+    num=int(input("enter number:"))
+    if is_prime(num):
+        print("prime")
+    else:
+        print("not prime")
+ma9,num):
+        if num%1==0:
+            return False
+    return True
+def main():
+    num =int(input("enter number:"))
+    if check_odd(num):
+        print("odd")
+    else:
+        print("not odd")
+main()        
+        
